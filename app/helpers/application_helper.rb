af255c011da32d47880c2362c57941ce6dc72314
Bui Huu Hoang <bhhoang1998@gmail.com>
2020-10-22 20:33:48 +0700

    Feature_active_menu_and_edit_layout_room

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     base_title = t "global.app_name"
     page_title.blank? ? base_title : [page_title, base_title].join(" | ")
   end
+
+  def current_path? path
+    request.path.eql?(path) ? "active_menu" : " "
+  end
+
+  def current_path_admin? path
+    request.path.eql?(path) ? "active nav-link" : "nav-link"
+  end
 end
